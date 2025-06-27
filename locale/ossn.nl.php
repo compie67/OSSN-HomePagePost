fc3d70e7adf19b240c341c08fd97e3f1eba1747e
Compie67 <compie67@gmail.com>
2025-06-27 10:29:47 +0200

    Create ossn.nl.php

---
+++ locale/ossn.nl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   HomePagePosts
+ * @author    Rafael Amorim <amorim@rafaelamorim.com.br>
+ * @adjustment and tweaking Eric Redegeld nlsociaal.nlsociaal
+ * @copyright (c) Rafael Amorim
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.rafaelamorim.com.br/
+ *
+ */
+ 
+
+$nl = array(
+    'homepageposts:option:public'     => 'ALLE BERICHTEN',
+    'homepageposts:option:friends'    => 'VRIENDEN',
+    'homepageposts:option:liked'      => 'MEEST GELIKET',
+    'homepageposts:option:likedmine'  => 'MIJN MEEST GELIKETE',
+);
+ossn_register_languages('nl', $nl);
