f7076a96e8cdfcb3ced9e989f42a3779dbe2e58c
Compie67 <compie67@gmail.com>
2025-06-27 10:28:51 +0200

    Create fontawesome.php

---
+++ plugins/default/css/fontawesome.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   HomePagePosts
+ * @author    Rafael Amorim <amorim@rafaelamorim.com.br>
+ * @adjustment and tweaking Eric Redegeld nlsociaal.nlsociaal
+ * @copyright (c) Rafael Amorim
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.rafaelamorim.com.br/
+ *
+ */
+ 
+echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />';
